fddab3db332c69b0994f85ff6494f015a0247bf3
higo.souza <higo.souza@vitru.com.br>
2024-05-17 20:50:22 -0300

    lista de carros basico
    lista de carros basico

---
+++ src/main/java/com/carros/domain/Carro.java
@@ -0,0 +1,27 @@
+package com.carros.domain;
+
+public class Carro {
+	private Long id;
+	private String nome;
+	
+	public Carro(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setId(String nome) {
+		this.nome = nome;
+	}
+}
