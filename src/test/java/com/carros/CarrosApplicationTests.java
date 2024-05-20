ff3e46a8db0bb6161bc52aa5f996b1fded0cb351
higo.souza <higo.souza@vitru.com.br>
2024-05-20 19:09:03 -0300

    testes com spring boot
    testes com spring boot

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -1,13 +1,19 @@
 package com.carros;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CarrosApplicationTests {
+public class CarrosApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void test1() {
 	}
 
+	@Test
+	public void test2() {
+	}
 }
