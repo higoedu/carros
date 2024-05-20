29284a43c8292eb01989f1e9647efc70b9e6ef83
higo.souza <higo.souza@vitru.com.br>
2024-05-20 19:17:14 -0300

    teste para inserir um carro
    teste para inserir um carro

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -2,15 +2,27 @@ package com.carros;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CarrosApplicationTests {
 
+	@Autowired
+	private CarroService service;
+	
 	@Test
 	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("esportivos");
+		
+		service.insert(carro);
 	}
 
 	@Test
