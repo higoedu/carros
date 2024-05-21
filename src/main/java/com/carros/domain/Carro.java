443d2e2d05a97dee1bbdbc9828b8d13c9b21c667
higo.souza <higo.souza@vitru.com.br>
2024-05-20 21:30:08 -0300

    preenchendo h2 com uma massa de testes
    preenchendo h2 com uma massa de testes

--- src/main/java/com/carros/domain/Carro.java
+++ src/main/java/com/carros/domain/Carro.java
@@ -14,7 +14,13 @@ public class Carro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
 	private String tipo;
+	private String descricao;
+	private String urlFoto;
+	private String urlVideo;
+	private String latitude;
+	private String longitude;
 
 }
--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -51,9 +51,29 @@ public class CarrosApplicationTests {
 	}
 
 	@Test
-	public void test2() {
+	public void testLista() {
 		List<CarroDTO> carros = service.getCarros();
 		
 		assertEquals(31, carros.size());
+	}
+	
+	@Test
+	public void testListaPorTipo() {
+		assertEquals(10, service.getCarrosByTipo("classicos").size());
+		assertEquals(10, service.getCarrosByTipo("esportivos").size());
+		assertEquals(10, service.getCarrosByTipo("luxo").size());
+
+		assertEquals(0, service.getCarrosByTipo("x").size());
+	}
+
+	@Test
+	public void testGet() {
+		Optional<CarroDTO> op = service.getCarroById(11L);
+		
+		assertTrue(op.isPresent());
+		
+		CarroDTO c = op.get();
+		
+		assertEquals("Ferrari FF", c.getNome());
 	}
 }
