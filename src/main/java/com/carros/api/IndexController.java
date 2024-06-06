e158738cbfe0a22faa1803f160d5a7cad56bd18c
higo.souza <higo.souza@vitru.com.br>
2024-06-05 21:30:47 -0300

    representa o usuário logado
    representa o usuário logado

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,5 +1,7 @@
 package com.carros.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -81,6 +83,11 @@ public class IndexController {
 	@GetMapping()
 	public String get() {
 		return "API dos Carros: ";
+	}
+	
+	@GetMapping("/userInfo")
+	public UserDetails userInfo(@AuthenticationPrincipal UserDetails user) {
+		return user;
 	}
 	
 }
