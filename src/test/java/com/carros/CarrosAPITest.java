a22f4a9e2425cd93367558e393ecc11d605ba744
higo.souza <higo.souza@vitru.com.br>
2024-06-06 19:47:32 -0300

    coloca autenticação
    coloca autenticação

--- src/test/java/com/carros/CarrosAPITest.java
+++ src/test/java/com/carros/CarrosAPITest.java
@@ -26,11 +26,11 @@ public class CarrosAPITest {
 	protected TestRestTemplate rest;
 	
 	private ResponseEntity<CarroDTO> getCarro(String url) {
-        return rest.withBasicAuth("user","123").getForEntity(url, CarroDTO.class);
+        return rest.withBasicAuth("user", "123").getForEntity(url, CarroDTO.class);
     }
 
     private ResponseEntity<List<CarroDTO>> getCarros(String url) {
-        return rest.exchange(
+        return rest.withBasicAuth("user", "123").exchange(
                 url,
                 HttpMethod.GET,
                 null,
@@ -46,7 +46,7 @@ public class CarrosAPITest {
         carro.setTipo("esportivos");
 
         // Insert
-        ResponseEntity response = rest.postForEntity("/api/v1/carros", carro, null);
+        ResponseEntity response = rest.withBasicAuth("admin", "123").postForEntity("/api/v1/carros", carro, null);
         System.out.println(response);
 
         // Verifica se criou
@@ -61,7 +61,7 @@ public class CarrosAPITest {
         assertEquals("esportivos", c.getTipo());
 
         // Deletar o objeto
-        rest.delete(location);
+        rest.withBasicAuth("user", "123").delete(location);
 
         // Verificar se deletou
         assertEquals(HttpStatus.NOT_FOUND, getCarro(location).getStatusCode());
