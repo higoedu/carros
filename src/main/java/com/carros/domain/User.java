cb56187e2035a129645c2e2e8e724b2a7f5bf591
higo.souza <higo.souza@vitru.com.br>
2024-06-05 22:25:20 -0300

    autenticação de usuário no banco de dados parte 1
    autenticação de usuário no banco de dados parte 1

---
+++ src/main/java/com/carros/domain/User.java
@@ -0,0 +1,29 @@
+package com.carros.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private String login;
+	private String senha;
+	private String email;
+	
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("123"));
+	}
+
+}
